6ed8a5da46c45ca39eca2944f36453e377bcd374
codebysubhan <154998920+codebysubhan@users.noreply.github.com>
2023-12-28 13:04:18 +0500

    db_layer_update

--- DB_Layer.py
+++ DB_Layer.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql_db
 import dbpopulate
+import random
 class DBManager:
     def __init__(self, filename:str, con=None, cur=None):
         if con is None:
@@ -30,13 +31,13 @@ class DBManager:
         data = self.cur.fetchall()
         try:
             if data[0][0] == 1:
-                print('Congratulations! ticket is available!', data)
+                print('Congratulations! ticket is valid!', data)
             else:
-                print(': ticket is not available! already sold!')
+                print(': ticket is not valid!')
         except:
             print('An error occurred!')
 
-    def getMatchDetailsFromDB(self, match_id):
+    def getMatchDetailsFromDB(self, match_id:int):
         self.cur.execute(f'''
         select match_id, schedule from matches where match_id = {match_id}
         ''')
@@ -58,13 +59,31 @@ class DBManager:
             timeofmatch = dbpopulate.datetime_converter(row[1])
             print(f'Match id = {id}\tSchedule = {timeofmatch}')
 
-    def registerFanInDB(self, name, email, phone):
-        # Logic to register a fan in the database
-        pass
+    def create_new_fan_id(self):
+        self.cur.execute('''
+        select fan_id from fans
+        ''')
+        data = self.cur.fetchall()
+        data = [i[0] for i in data]
+        new_id = random.choice(list(set(range(1, 500)) - set(data)))
+        return new_id
+    
+    def registerFanInDB(self, name:str, email:str, phone:str):
+        self.cur.execute('''
+            INSERT INTO fans (fan_id, name, email, phone)
+            VALUES (?, ?, ?, ?)
+        ''', (self.create_new_fan_id(), name, email, phone))
+        self.con.commit()
+        print('Fan added success!!!')
 
-    def getFanDetailsFromDB(self, fan_id):
-        # Logic to retrieve fan details based on fan_id
-        pass
+    def getFanDetailsFromDB(self, fan_id:int):
+        self.cur.execute(f'''
+        select name, email, phone from fans where fan_id = {fan_id}
+        ''')
+        data = self.cur.fetchall()
+        out_str = f'Fan id = {fan_id}\nName: {data[0][0]}\nEmail: {data[0][1]}\nPhone: {data[0][2]}'
+        print(out_str)
+        return data[0]
 
     def getFanPurchaseHistoryFromDB(self, fan_id):
         # Logic to retrieve fan's purchase history from the database
@@ -74,14 +93,27 @@ class DBManager:
         # Logic to process ticket refund in the database
         pass
 
-    def getStadiumDetailsFromDB(self, match):
-        # Logic to retrieve stadium details based on match
-        pass
+    def getStadiumDetailsFromDB(self, match_id:int):
+        query = '''
+        SELECT stadiums.stadium_id, stadiums.name, stadiums.capacity
+        FROM matches
+        JOIN stadiums ON matches.stadium_id = stadiums.stadium_id
+        WHERE matches.match_id = ?
+        '''
+        self.cur.execute(query, (match_id,))
+        result = self.cur.fetchone()
+        out_str = f'MATCH_ID = {match_id}\nStadium Nmae: {result[1]}\nStadium Capacity: {result[2]}'
+        print(out_str)
 
-    def getStadiumCapacityFromDB(self, stadium):
-        # Logic to retrieve stadium capacity based on stadium
-        pass
-
+    def getStadiumCapacityFromDB(self, stadium_id:int):
+        query = '''
+        select capacity from stadiums where stadium_id = ?
+        '''
+        self.cur.execute(query, (stadium_id,))
+        result = self.cur.fetchone()[0]
+        out_str = f'Stadium Capacity for Stadium ID = {stadium_id} is:\t{result}'
+        print(out_str)
+        return result
 
 def main():
     filename = "database.db"
@@ -90,6 +122,9 @@ def main():
     layer.checkTicketValidityInDB(2)
     layer.getMatchDetailsFromDB(1)
     layer.getMatchScheduleFromDB()
-
+    # layer.registerFanInDB("Abdullah", "abd@gmail.com", "03334568527")
+    layer.getFanDetailsFromDB(1)
+    layer.getStadiumDetailsFromDB(20)
+    layer.getStadiumCapacityFromDB(15)
 if __name__ == "__main__":
     main()
--- dbpopulate.py
+++ dbpopulate.py
@@ -1,8 +1,8 @@
 # importing necessary files
-from db_layer import DBManager
-# from bl_layer import TicketManager, MatchManager, FanManager, RefundManager, StadiumManager, TransactionManager
-import bl_layer as bl
-from integ_layer import DataIntegration
+# from db_layer import DBManager
+# # from bl_layer import TicketManager, MatchManager, FanManager, RefundManager, StadiumManager, TransactionManager
+# import bl_layer as bl
+# from integ_layer import DataIntegration
 
 # importing necessary modules
 import sqlite3 as sql_db
@@ -56,49 +56,123 @@ def createtickets():
 # alter table tickets add column 
 # ''')
 
-def creatematches():
+def create_matches():
     cur.execute('''
         CREATE TABLE matches
         (MATCH_ID INT PRIMARY KEY NOT NULL,
-        SCHEDULE DATETIME
+        SCHEDULE DATETIME,
+        STADIUM_ID INT
         );
     ''')
     cur.execute('''
-        INSERT INTO matches (match_id, schedule)
-        values (1, '2023-12-28 12:30:00')
+        INSERT INTO matches (match_id, schedule, stadium_id)
+        values (1, '2023-12-28 12:30:00', 1)
     ''')
     matches_data = [
-        (2, '2023-12-29 15:45:00'),
-        (3, '2023-12-30 18:00:00'),
-        (4, '2023-12-31 20:15:00'),
-        (5, '2024-01-01 14:00:00'),
-        (6, '2024-01-02 16:30:00'),
-        (7, '2024-01-03 19:45:00'),
-        (8, '2024-01-04 21:00:00'),
-        (9, '2024-01-05 12:00:00'),
-        (10, '2024-01-06 14:30:00'),
-        (11, '2024-01-07 16:45:00'),
-        (12, '2024-01-08 18:30:00'),
-        (13, '2024-01-09 20:15:00'),
-        (14, '2024-01-10 12:00:00'),
-        (15, '2024-01-11 14:30:00'),
-        (16, '2024-01-12 17:00:00'),
-        (17, '2024-01-13 19:45:00'),
-        (18, '2024-01-14 21:30:00'),
-        (19, '2024-01-15 12:30:00'),
-        (20, '2024-01-16 15:00:00')
+        (2, '2023-12-29 15:45:00', 2),
+        (3, '2023-12-30 18:00:00', 3),
+        (4, '2023-12-31 20:15:00', 4),
+        (5, '2024-01-01 14:00:00', 5),
+        (6, '2024-01-02 16:30:00', 6),
+        (7, '2024-01-03 19:45:00', 7),
+        (8, '2024-01-04 21:00:00', 8),
+        (9, '2024-01-05 12:00:00', 9),
+        (10, '2024-01-06 14:30:00', 10),
+        (11, '2024-01-07 16:45:00', 11),
+        (12, '2024-01-08 18:30:00', 12),
+        (13, '2024-01-09 20:15:00', 13),
+        (14, '2024-01-10 12:00:00', 14),
+        (15, '2024-01-11 14:30:00', 15),
+        (16, '2024-01-12 17:00:00', 16),
+        (17, '2024-01-13 19:45:00', 17),
+        (18, '2024-01-14 21:30:00', 18),
+        (19, '2024-01-15 12:30:00', 19),
+        (20, '2024-01-16 15:00:00', 20)
     ]
+
     cur.executemany('''
-    insert into matches (match_id, schedule)
-                    values(?, ?)
+        INSERT INTO matches (match_id, schedule, stadium_id)
+        VALUES (?, ?, ?)
     ''', matches_data)
-# creatematches()
+# create_matches()
+
 def datetime_converter(result):
     datetime_str = result
     datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
     return datetime_obj
 
+def create_fans():
+    cur.execute('''
+        CREATE TABLE fans
+        (FAN_ID INT PRIMARY KEY NOT NULL,
+        NAME TEXT,
+        EMAIL TEXT,
+        PHONE VARCHAR(12)
+        );
+    ''')
+    fans_data = [
+    (1, "Subhan Ali", "subhanali1212w@gmail.com", "03084072295"),
+    (2, "John Doe", "john.doe@example.com", "1234567890"),
+    (3, "Jane Doe", "jane.doe@example.com", "9876543210"),
+    (4, "Alice Smith", "alice.smith@example.com", "555-1234"),
+    (5, "Bob Johnson", "bob.johnson@example.com", "555-5678"),
+    (6, "Emily Brown", "emily.brown@example.com", "555-9876"),
+    (7, "Alex Wilson", "alex.wilson@example.com", "333-1111"),
+    (8, "Grace Turner", "grace.turner@example.com", "444-5678"),
+    (9, "Ryan Miller", "ryan.miller@example.com", "777-8888"),
+    (10, "Sophia Davis", "sophia.davis@example.com", "999-0000"),
+    (11, "Mia Garcia", "mia.garcia@example.com", "111-2222"),
+    (12, "Liam White", "liam.white@example.com", "444-3333"),
+    (13, "Olivia Martinez", "olivia.martinez@example.com", "777-6666"),
+    (14, "Ethan Hall", "ethan.hall@example.com", "888-4444"),
+    (15, "Ava Taylor", "ava.taylor@example.com", "333-9999"),
+    (16, "Noah Harris", "noah.harris@example.com", "555-5555"),
+    (17, "Isabella Clark", "isabella.clark@example.com", "666-1111"),
+    (18, "James Turner", "james.turner@example.com", "999-8888"),
+    (19, "Emma Moore", "emma.moore@example.com", "222-4444"),
+    (20, "William Allen", "william.allen@example.com", "888-9999")
+    # Add more data as needed
+    ]
+    cur.executemany('''
+    insert into fans (fan_id, name, email, phone)
+                    values(?, ?, ?, ?)
+    ''', fans_data)
+# create_fans()
 
+def create_stadiums():
+    cur.execute('''
+        CREATE TABLE stadiums
+        (STADIUM_ID INT PRIMARY KEY NOT NULL,
+        NAME TEXT,
+        CAPACITY INT    );
+    ''')
+    stadium_data = [
+    (1, 'Stadium A', 50000),
+    (2, 'Stadium B', 60000),
+    (3, 'Stadium C', 70000),
+    (4, 'Stadium D', 80000),
+    (5, 'Stadium E', 90000),
+    (6, 'Stadium F', 100000),
+    (7, 'Stadium G', 110000),
+    (8, 'Stadium H', 120000),
+    (9, 'Stadium I', 130000),
+    (10, 'Stadium J', 140000),
+    (11, 'Stadium K', 150000),
+    (12, 'Stadium L', 160000),
+    (13, 'Stadium M', 170000),
+    (14, 'Stadium N', 180000),
+    (15, 'Stadium O', 190000),
+    (16, 'Stadium P', 200000),
+    (17, 'Stadium Q', 210000),
+    (18, 'Stadium R', 220000),
+    (19, 'Stadium S', 230000),
+    (20, 'Stadium T', 240000),
+    ]
+    cur.executemany('''
+    INSERT INTO stadiums (STADIUM_ID, NAME, CAPACITY) 
+    VALUES (?, ?, ?)
+    ''', stadium_data)
+# create_stadiums()
 
 
 con.commit()
